28b15594c473398b646d04f686536cb7264545f3
rajatmw1999 <rajatis1999@gmail.com>
2019-07-27 17:58:21 +0530

    using heroku

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/failure',function(req,res){
 
 });
 
-app.listen(3000,function(){
+app.listen(process.env.port || 3000,function(){
   console.log("Server is running on port 3000");
 });
 
